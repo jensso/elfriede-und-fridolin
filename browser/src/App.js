ad3daa5e7f4dc42e0a6bb6f8e8011d2a7ffcf733
jens soltwedel <jens.soltwedel@hotmail.com>
2019-05-27 20:03:20 +0200

    BottomNav with styling approach

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Footer} from './components/footer.js';
+import { Footer } from './components/footer.js';
 
 
 
--- browser/src/components/footer.js
+++ browser/src/components/footer.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import Impressum from './impressum.js';
+import MyImpressum from './impressum.js';
 import AllGesBed from './agb.js';
 import Datenschutz from './datenschutz.js';
 import Widerruf from './widerruf.js';
+import { styled } from '@material-ui/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
 
+const MyBottomNavigation= styled(BottomNavigation)({
+  background: 'lightblue',
+  border: '0.2vw solid silver',
+  borderRadius: '1vw',
+});
 
 export class Footer extends React.Component {
+
   render() {
     return(
       <>
-        <Impressum />
-        <AllGesBed />
-        <Datenschutz />
-        <Widerruf />
+      <MyBottomNavigation>
+          <MyImpressum />
+          <AllGesBed />
+          <Datenschutz />
+          <Widerruf />
+      </MyBottomNavigation>
       </>
     )
   }
