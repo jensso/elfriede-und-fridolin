872e077f377ee324c6e43535159c996957e4fcee
Miladcj <36736436+Miladcj@users.noreply.github.com>
2019-05-28 11:05:41 +0200

    final chenges on schemas

--- models/patternsModel.js
+++ models/patternsModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const patternSchema = new mongoose.Schema({
   Produktname: {type: String, required: true},
-  Produktnummer: {type: Number, required: false},
+  Produktnummer: {type: String, required: false},
   Produktfotos: {type: String, required: true},
-  Preis: {type: Number, required: true},
+  Preis: {type: String, required: true},
   Beispielfotos: {type: String, required: true},
   Produktbeschreibung: {type: String, required: true}
 }, {versionKey: false})
