10b349027876e8dab9c01b362e3b1bd51ca195ff
jens soltwedel <jens.soltwedel@hotmail.com>
2019-06-13 10:01:23 +0200

    clothesModel added

--- routes/clothesRoutes.js
+++ routes/clothesRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const clothesRoutes = express.Router();
-// const { addClothes, getAllClothes, deleteClothesByID, updateClothesByID } = require('../middlewares/clothesRoutesHandler');
+const { addClothes, getAllClothes, deleteClothesByID, updateClothesByID } = require('../middlewares/clothesRoutesHandler');
 
 
-// clothesRoutes.post('/addClothes', addClothes);
-// clothesRoutes.get('/getAllClothes', getAllClothes);
-// clothesRoutes.delete('/:produktnummer', deleteClothesByID);
-// clothesRoutes.put('/:produktnummer', updateClothesByID);
+clothesRoutes.post('/addClothes', addClothes);
+clothesRoutes.get('/getAllClothes', getAllClothes);
+clothesRoutes.delete('/:produktnummer', deleteClothesByID);
+clothesRoutes.put('/:produktnummer', updateClothesByID);
 
 
 module.exports = clothesRoutes;
