41d2a37abe4892256e4a850f45d10624d81ab4b4
Miladcj <36736436+Miladcj@users.noreply.github.com>
2019-06-18 23:59:15 +0200

    emailConfirm refactored

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const emailRoutes = express.Router();
 const userModel = require('../models/usersModel');
+const confirmEmail = require('../middlewares/emailHandler');
 
-emailRoutes.get('/confirm/:stamp', async (req, res, next) => {
-  try {
-    const user = await userModel.findOneAndUpdate({temporaryToken: req.params.stamp}, {$set: {isConfirmed: true}, $unset: {temporaryToken: ''}});
-    user ? res.redirect('https://www.google.de') : res.status(404).json({message: 'User was not found'})
-  } catch(error) {
-    next(error);
-  }
-})
+emailRoutes.get('/confirm/:stamp', confirmEmail)
 
 module.exports = emailRoutes;
